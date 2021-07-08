68bcdd9e2e14d2de9b5899227707707a76912c45
quanghung97 <hungcan1997@gmail.com>
2021-07-08 13:36:54 +0700

    chore: init project

---
+++ nuxt.config.js
@@ -0,0 +1,76 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - proof-of-concept-fe-spa',
+    title: 'proof-of-concept-fe-spa',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+    '@nuxtjs/style-resources',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/vuetify/variables.scss'],
+    optionsPath: './vuetify.options.js',
+    treeShake: true
+  },
+
+  styleResources: {
+    scss: [
+        '~/assets/color.scss'
+    ],
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/buttons.vue
@@ -0,0 +1,98 @@
+<template>
+<div>
+  <v-row
+    class="mb-5 mt-5"
+    align="center"
+    justify="space-around"
+  >
+    <v-btn depressed text large>
+      large
+    </v-btn>
+    <v-btn depressed large class="gradient shades--text text--black">
+      default
+    </v-btn>
+    <v-btn
+      depressed
+      large
+      class="yellow yellow-5 shades--text text--black"
+    >
+      hover
+    </v-btn>
+    <v-btn
+      depressed
+      large
+    >
+      pressed
+    </v-btn>
+    <v-btn
+      depressed
+      disabled
+      large
+    >
+      disabled
+    </v-btn>
+  </v-row>
+  <v-row
+    class="mb-5 mt-5"
+    align="center"
+    justify="space-around"
+  >
+    <v-btn depressed text>
+      Text
+    </v-btn>
+    <v-btn depressed>
+      Normal
+    </v-btn>
+    <v-btn
+      depressed
+      color="primary"
+    >
+      Primary
+    </v-btn>
+    <v-btn
+      depressed
+      color="error"
+    >
+      Error
+    </v-btn>
+    <v-btn
+      depressed
+      disabled
+    >
+      Disabled
+    </v-btn>
+  </v-row>
+  <v-row
+    class="mb-5 mt-5"
+    align="center"
+    justify="space-around"
+  >
+    <v-btn depressed text>
+      Text
+    </v-btn>
+    <v-btn depressed>
+      Normal
+    </v-btn>
+    <v-btn
+      depressed
+      color="primary"
+    >
+      Primary
+    </v-btn>
+    <v-btn
+      depressed
+      color="error"
+    >
+      Error
+    </v-btn>
+    <v-btn
+      depressed
+      disabled
+    >
+      Disabled
+    </v-btn>
+  </v-row>
+</div>
+</template>
+<script>
+</script>
